768e7776a204d50efe4e5cf2d91a3fd41e09ca81
Harinivelu <harinivelu45@gmail.com>
2025-05-07 17:59:19 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,18 @@
+var a=10;     // variable concept  non primitive datatypes
+var b=20;
+var c= 30;
+var d=a+b+c;
+
+console.log(d);
+
+// let concept can't redeclare but can reassign
+let a1= 10;
+let a2=20;
+console.log(a1);
+console.log(a2);
+
+
+//Constant Concept
+const c1 = "playwright";
+console.log(c1);
+
